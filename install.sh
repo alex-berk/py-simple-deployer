f4448a4d9026f4976ed6d8bc40421d4bee192cf5
Alex Berk <aleks.berkov@gmail.com>
2024-01-26 23:58:08 +0200

    generate service file with provided inputs

--- install.sh
+++ install.sh
@@ -15,12 +15,26 @@ function get_user_input() {
 	echo "$inp"
 }
 
-PROJECTS_DIR=$(get_user_input "Path to the directory with projects" "~/projects")
-SETTINGS_FILENAME=$(get_user_input "Name for the file with deploy settings"  "lhs-deployer-settings.json")
-HOST=$(get_user_input "Host"  "0.0.0.0")
-PORT=$(get_user_input "Port"  "8069")
+PYTHON_VERSION=$(get_user_input "Do you want to use python 3 or 2?")
+PROJECTS_DIR=$(get_user_input "Path to the directory with projects" "$(pwd)/projects")
+SETTINGS_FILENAME=$(get_user_input "Name for the file with deploy settings" "lhs-deployer-settings.json")
+HOST=$(get_user_input "Host" "0.0.0.0")
+PORT=$(get_user_input "Port" "8069")
 BRANCH_NAME=$(get_user_input "Branch to checkout (optional)")
 UUID=$(uuidgen)
 
+VERSIONS_2=("2", "2.7" "two")
+if [[ ${VERSIONS_2[@]} =~ $PYTHON_VERSION ]]; then
+	PYTHON_VERSION="2.7"
+	PYTHON_EXECUTABLE=$(which python)
+else
+	PYTHON_VERSION="3.11"
+	PYTHON_EXECUTABLE=$(which python3)
+fi
+DEPLOYER_FILE="py-simple-deployer-p$PYTHON_VERSION.py"
+
 echo "Provided inputs:"
-echo $PROJECTS_DIR $SETTINGS_FILENAME $BRANCH_NAME $HOST:$PORT $UUID
+echo $DEPLOYER_FILE $PYTHON_EXECUTABLE $PROJECTS_DIR $SETTINGS_FILENAME $BRANCH_NAME $HOST:$PORT $UUID
+
+SERVICE_FILE_CONTENT="[Unit]\nDescription=SimpleDeploy Daemon\nAfter=network-online.target\n\n[Service]\nType=simple\nEnvironment=\"PROJECTS_DIR=$PROJECTS_DIR\"\nEnvironment=\"SETTINGS_FILENAME=$SETTINGS_FILENAME\"\nEnvironment=\"BRANCH_NAME=$BRANCH_NAME\"\nEnvironment=\"HOST=$HOST\"\nEnvironment=\"PORT=$PORT\"\nEnvironment=\"UUID=$UUID\"\nExecStart=$PYTHON_EXECUTABLE $(pwd)$DEPLOYER_FILE\nRestart=on-failure\n\n[Install]\nWantedBy=multi-user.target\n"
+echo $SERVICE_FILE_CONTENT > tempfile.service
